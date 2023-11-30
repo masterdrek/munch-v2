c1581b1704896233b622fcb33025ef1bc9e4c2cf
derek <dereklee1124@gmail.com>
2023-11-29 18:14:46 -0800

    changed to prepared statements and also working on check user method

--- src/main/java/munch/Users.java
+++ src/main/java/munch/Users.java
@@ -1,14 +1,30 @@
 package munch;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Users
 {
     public static void addUser(Connection con, String name, String username, String password) throws SQLException
     {
-        Statement statement = con.createStatement();
-        statement.executeUpdate("insert into Users(name, username, password) values('"+name+"', '" + username + "', '" + password + "');");
+        String insertString = "insert into Users(name, username, password) values(?,?,?)";
+        PreparedStatement insertUser = con.prepareStatement(insertString);
+        insertUser.setString(1, name);
+        insertUser.setString(2, username);
+        insertUser.setString(3,password);
+        insertUser.executeUpdate();
+    }
+    public static void checkUser(Connection con, String username, String password) throws SQLException
+    {
+        String selectString = "select userID, name, username, password from Users where username = ?";
+        PreparedStatement selectUser = con.prepareStatement(selectString);
+        selectUser.setString(1, username);
+        ResultSet rs = selectUser.executeQuery();
+
+        while (rs.next()) {
+            String readUsername = rs.getString("username");
+            String readPassword = rs.getString("password");
+            System.out.println("userid : " + readUsername);
+            System.out.println("username : " + readPassword);
+        }
+
     }
 }
