634714284601795acd704423eb7a86ef3051c315
derek <dereklee1124@gmail.com>
2023-11-30 22:16:12 -0800

    added method to get ratings and reviews for restaurant

--- src/main/java/munch/Ratings.java
+++ src/main/java/munch/Ratings.java
@@ -1,14 +1,59 @@
 package munch;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javafx.util.Pair;
+
+import java.sql.*;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class Ratings
 {
+
     public static void addRating(Connection con, int restID, int rating, int userID) throws SQLException
     {
         Statement statement = con.createStatement();
         statement.executeUpdate("insert into Ratings(restID, rating, userID) values ("+restID+ "," + rating + "," + userID + ");");
+    }
+
+    public static List<Pair<Rating, Review>> getRatingsAndReviewsForRestaurant(Connection con, int restID) throws  SQLException
+    {
+        String selectRatingsString = "select ratingID, restID, rating, userID from Ratings where restID = ?";
+        PreparedStatement selectRatings = con.prepareStatement(selectRatingsString);
+        selectRatings.setInt(1, restID);
+        ResultSet ratingsResults = selectRatings.executeQuery();
+
+        List<Pair<Rating, Review>> ratingsAndReviews = new ArrayList<>();
+        while(ratingsResults.next())
+        {
+            String selectReviewString = "select ratingID, restID, review from Reviews where ratingID = ?";
+            PreparedStatement selectReview = con.prepareStatement(selectReviewString);
+            selectReview.setInt(1, ratingsResults.getInt("ratingID"));
+            ResultSet reviewResult = selectReview.executeQuery();
+            Rating rating = new Rating(ratingsResults.getInt("ratingID"),
+                                       ratingsResults.getInt("restID"),
+                                       ratingsResults.getInt("rating"),
+                                       ratingsResults.getInt("userID"));
+            Review review = null;
+
+            if(reviewResult.isBeforeFirst())
+            {
+
+                if (reviewResult.next()) {
+                    System.out.println(reviewResult.getString("review"));
+                    review = new Review(
+                            reviewResult.getInt("ratingID"),
+                            reviewResult.getInt("restID"),
+                            reviewResult.getString("review")
+                    );
+                } else {
+                    System.out.println("No reviews found.");
+                }
+            }
+            Pair<Rating, Review> entry = new Pair<>(rating,review);
+            ratingsAndReviews.add(entry);
+        }
+        return ratingsAndReviews;
     }
 }
